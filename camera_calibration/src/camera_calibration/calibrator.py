c11de4bb7b34e5a3c2c2232169346bee7f006088
tlzh0001 <ztl1998@berkeley.edu>
2023-12-21 16:46:12 -0800

    back to correct calibration parameter

--- camera_calibration/src/camera_calibration/calibrator.py
+++ camera_calibration/src/camera_calibration/calibrator.py
@@ -465,7 +465,7 @@ class Calibrator():
         progress = [min((hi - lo) / r, 1.0) for (lo, hi, r) in zip(min_params, max_params, self.param_ranges)]
         # If we have lots of samples, allow calibration even if not all parameters are green
         # TODO Awkward that we update self.goodenough instead of returning it
-        self.goodenough = (len(self.db) >= 20) or all([p == 0.8 for p in progress])
+        self.goodenough = (len(self.db) >= 40) or all([p == 1.0 for p in progress])
 
         return list(zip(self._param_names, min_params, max_params, progress))
 
